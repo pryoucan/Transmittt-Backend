8972088ef8e17c0ae3c95355038e733381dca503
pryoucan <priyanshu123543@gmail.com>
2026-06-04 15:01:17 +0530

    feat: increase the file size limit

--- src/server.ts
+++ src/server.ts
@@ -39,7 +39,7 @@ app.use(cors({
     origin: process.env.FRONTEND_URL
 }));
 
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '150mb' }));
 
 // Health checkpoint API
 app.get("/api/health", (req, res) => {
